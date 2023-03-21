27a6308a112dea0b416c4d54ddee506601ad8049
matisbyar <matis.byar.06@icloud.com>
2023-03-21 12:06:59 +0100

    Récupération des données de la BD lorsque la fenêtre est chargée

--- src/scripts/initialisation.js
+++ src/scripts/initialisation.js
@@ -1,12 +1,33 @@
-const listeAdherents2 = document.getElementById('listeAdherents');
-
-
 async function recupererLivresDisponibles() {
-    fetch(`php/Controller/ControllerLivre.php?action=readAll`)
-        .then(response => response.json())
-        .then(response => Emprunt.tableauVersObjet(response))
-        .then(response => ajouterLivresDisponibles(response))
-        .catch(error => console.log(error));
+    try {
+        let req = await fetch(`php/Controller/ControllerLivre.php?action=readAllDisponibles`);
+        let data = await req.json();
+        const tabReq = data.map(async function (element) {
+            return new Livre(element.idLivre, element.titreLivre);
+        });
+        for (const element of tabReq) {
+            let data = await element;
+            listeLivresDisponibles.insertAdjacentElement("beforeend", creerElement(data.idLivre + " - " + data.titreLivre));
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+async function recupererLivresEmpruntes() {
+    try {
+        let req = await fetch(`php/Controller/ControllerLivre.php?action=readAllEmpruntes`);
+        let data = await req.json();
+        const tabReq = data.map(async function (element) {
+            return new Livre(element.idLivre, element.titreLivre);
+        });
+        for (const element of tabReq) {
+            let data = await element;
+            livresEmpruntes.insertAdjacentElement("beforeend", creerElement(data.idLivre + " - " + data.titreLivre));
+        }
+    } catch (error) {
+        console.log(error);
+    }
 }
 
 /**
@@ -14,8 +35,8 @@ async function recupererLivresDisponibles() {
  * Ajout dans l'interface lors du chargement de la page
  * @returns {Promise<void>}
  */
-async function recupererAdherents(){
-    try{
+async function recupererAdherents() {
+    try {
         let req = await fetch(`php/Controller/ControllerAdherent.php?action=readAll`);
         let data = await req.json();
         const tabReq = data.map(async function (element) {
@@ -23,17 +44,21 @@ async function recupererAdherents(){
         });
         for (const element of tabReq) {
             let data = await element;
-            listeAdherents.insertAdjacentElement("beforeend", creerElementAdherent(data.idAdherent, data.nomAdherent));
+            listeAdherents.insertAdjacentElement("beforeend", creerElement(data.idAdherent + " - " + data.nomAdherent));
         }
-    }catch(error) {
+    } catch (error) {
         console.log(error);
     }
 }
-window.addEventListener("load", recupererAdherents);
 
-
-function creerElementAdherent(idAdherent, adherentsNom) {
+function creerElement(element) {
     let nouveauAdherent = document.createElement("li");
-    nouveauAdherent.innerText = idAdherent + '-' + adherentsNom;
+    nouveauAdherent.innerText = element;
     return nouveauAdherent;
 }
+
+window.addEventListener("load", () => {
+    recupererAdherents();
+    recupererLivresEmpruntes();
+    recupererLivresDisponibles();
+});
