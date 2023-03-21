ef4ff913dac4c253f8198d7a1a7ffef197784f9d
matisbyar <matis.byar.06@icloud.com>
2023-03-21 12:05:17 +0100

    Ajout de nouvelles requêtes - Une requête sur la vue livresNonEmpruntes - Une requête sur les livres qui sont empruntés (ainsi que l'adhérent concerné)

--- src/php/Model/ModelLivre.php
+++ src/php/Model/ModelLivre.php
@@ -4,8 +4,38 @@ require_once "Model.php";
 require_once "ModelAdherent.php";
 require_once "ModelEmprunt.php";
 
-class ModelLivre extends Model {
+class ModelLivre extends Model
+{
     static $object = "livre";
     static $primary = "idLivre";
 
+    public static function selectDisponibles()
+    {
+        try {
+            $pdo = self::$pdo;
+            $class_name = 'ModelLivre';
+            $sql = "SELECT * from livresNonEmpruntes";
+            $rep = $pdo->query($sql);
+            $rep->setFetchMode(PDO::FETCH_CLASS, $class_name);
+            return $rep->fetchAll();
+        } catch (PDOException $e) {
+            echo $e->getMessage(); // affiche un message d'erreur
+            die();
+        }
+    }
+
+    public static function selectEmpruntes()
+    {
+        try {
+            $pdo = self::$pdo;
+            $class_name = 'ModelLivre';
+            $sql = "SELECT e.idLivre, titreLivre, idAdherent from emprunt e JOIN livre l ON l.idLivre = e.idLivre";
+            $rep = $pdo->query($sql);
+            $rep->setFetchMode(PDO::FETCH_CLASS, $class_name);
+            return $rep->fetchAll();
+        } catch (PDOException $e) {
+            echo $e->getMessage(); // affiche un message d'erreur
+            die();
+        }
+    }
 }
