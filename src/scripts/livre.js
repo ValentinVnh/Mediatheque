cf34ce563fae0beb3e76007868b4da493eb66619
ValentinVnh <92478691+ValentinVnh@users.noreply.github.com>
2023-03-21 11:05:25 +0100

    - Ajout d'un adherent dans la base de données et sont affichage - Ajout d'un livre dans la base de données et sont affichage

--- src/scripts/livre.js
+++ src/scripts/livre.js
@@ -1,12 +1,13 @@
-const nouveauLivreTitre = document.getElementById("titreLivre");
-const ajouterLivre = document.getElementById("ajouterLivre");
-
 class Livre {
     constructor(idLivre, titreLivre) {
         this.titreLivre = titreLivre;
         this.idLivre = titreLivre;
     }
 }
+
+const nouveauLivreTitre = document.getElementById("titreLivre");
+const ajouterLivre = document.getElementById("ajouterLivre");
+const listeLivresDisponibles = document.getElementById("listeLivresDisponibles");
 
 /**
  * Ajoute un nouveau livre dans la base de données
@@ -15,7 +16,18 @@ function ajouterUnLivre() {
     fetch(`php/Controller/ControllerLivre.php?action=create&titre=${nouveauLivreTitre.value}`)
         .then(response => response.json())
         .catch(error => console.log(error));
+    listeLivresDisponibles.insertAdjacentElement("beforeend", creerElementLivre(nouveauLivreTitre));
     nouveauLivreTitre.value = "";
 }
-
 ajouterLivre.addEventListener("click", ajouterUnLivre);
+
+/**
+ * Crée un livre sous format HTML.
+ * @param {Livre} livre
+ * @returns Un livre format HTML
+ */
+function creerElementLivre(livre) {
+    let nouveauLivre = document.createElement("li");
+    nouveauLivre.innerText = livre.value;
+    return nouveauLivre;
+}
